d97aa2ea47d364fd9aad139dfffc1009a7e2ed17
gchamb <gianttechambers@gmail.com>
2023-03-15 20:26:02 -0400

    build: :heavy_plus_sign: add ui components and other depends

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "aa-main": "#cf9fff",
+        "aa-0": "#f5ecff",
+        "aa-1": "#e9d2ff",
+        "aa-2": "#dcb9ff",
+        "aa-3": "#c286ff",
+        "aa-4": "#b66cff",
+        "aa-5": "#a953ff",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 };
