1cf4f5fa0c1ecf02d8363013dc6a284db1dc99d4
gchamb <gianttechambers@gmail.com>
2023-03-24 16:24:47 -0400

    feat: :construction: jikan show more wip

--- src/lib/jikan/helpers.ts
+++ src/lib/jikan/helpers.ts
@@ -3,7 +3,11 @@ import { Sections } from "../types";
 import { JikanResponse } from "./types";
 
 const jikan = new Jikan();
-export function getSection(sectionKey: keyof typeof Sections, page = 1): Promise<JikanResponse> {
-    const section = Sections[sectionKey];
-    return jikan.getTopAnimes("tv", section, page);
+
+export function getSection(
+  sectionKey: keyof typeof Sections,
+  page: number
+): Promise<JikanResponse> {
+  const section = Sections[sectionKey];
+  return jikan.getTopAnimes("tv", section, page);
 }
--- src/pages/animes/index.tsx
+++ src/pages/animes/index.tsx
@@ -1,22 +1,33 @@
 import AnimeResults from "@/components/anime-results";
-import { JikanResponse } from "@/lib/jikan/types";
+import { useAnimes } from "@/hooks/hooks";
+import { isSection } from "@/lib/types";
+
+// import { JikanResponse } from "@/lib/jikan/types";
 import { Loader2 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+// import { useEffect, useState } from "react";
 
 export default function Animes() {
-	const [data, setData] = useState<JikanResponse>();
+  const [query, setQuery] = useState<string | undefined>();
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
+  const { loading, data, error } = useAnimes({
+    query,
+    page,
+  });
 
-    },[])
+  if (error !== "") {
+    return <p>{error}</p>;
+  }
 
-	if (data === undefined) {
-		return <Loader2 className="w-4 h-4 animate-spin" />;
-	}
+  if (loading || data === undefined) {
+    return <Loader2 className="w-4 h-4 animate-spin" />;
+  }
 
-	return (
-		<div>
-			<AnimeResults data={data.data} />
-		</div>
-	);
+  return (
+    <div>
+      <AnimeResults data={data.data} />
+    </div>
+  );
 }
