1ed8b7e3192e3e00397188231895e8d9037852e5
Giantte <gchambe2@emich.edu>
2023-03-26 17:04:14 -0400

    refactor: :recycle: adjust select size, add full screen component, and prettier

--- src/hooks/jikan.tsx
+++ src/hooks/jikan.tsx
@@ -5,48 +5,46 @@ import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 export type FetchHookReturn<T> = {
-  loading: boolean;
-  error: string;
-  data: T;
+	loading: boolean;
+	error: string;
+	data: T;
 };
 
-export function useSectionAnimes(
-  type: string | undefined,
-  page: number
-): FetchHookReturn<JikanResponse | undefined> {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<JikanResponse | undefined>();
-  const [error, setError] = useState("");
+export function useSectionAnimes(type: string | undefined, page: number): FetchHookReturn<JikanResponse | undefined> {
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState<JikanResponse | undefined>();
+	const [error, setError] = useState("");
 
-  useEffect(() => {
-    const fetchAnimes = async () => {
-      try {
-        setLoading(true);
+	useEffect(() => {
+		const fetchAnimes = async () => {
+			try {
+				setLoading(true);
 
-        if (type !== undefined) {
-          // make sure the type is valid
-          if (!isSection(type)) {
-            throw new Error("Unacceptable anime type.");
-          }
+				if (type !== undefined) {
+					// make sure the type is valid
+					if (!isSection(type)) {
+						throw new Error("Unacceptable anime type.");
+					}
 
-          const jikanResponse = await getSection(type, page);
-          setData(jikanResponse);
-        } else {
-          throw new Error("You need an anime type.");
-        }
-      } catch (err) {
-        if (err instanceof AxiosError || err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError(String(err));
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
+					const jikanResponse = await getSection(type, page);
+					setData(jikanResponse);
+					setError("");
+				} else {
+					throw new Error("You need an anime type.");
+				}
+			} catch (err) {
+				if (err instanceof AxiosError || err instanceof Error) {
+					setError(err.message);
+				} else {
+					setError(String(err));
+				}
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    fetchAnimes();
-  }, [page, type]);
+		fetchAnimes();
+	}, [page, type]);
 
-  return { loading, error, data };
+	return { loading, error, data };
 }
--- src/pages/animes/index.tsx
+++ src/pages/animes/index.tsx
@@ -1,188 +1,180 @@
 import AnimeResults from "@/components/anime-results";
+import React, { useMemo, useState } from "react";
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useSectionAnimes } from "@/hooks/jikan";
 import { isSection } from "@/lib/types";
 import { getGenres } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
+import { FullScreen } from "@/components/full-screen";
 
 export default function Animes() {
-  const [page, setPage] = useState(1);
-  const [filters, setFilters] = useState<{
-    genres: string[];
-    episodes: "1-12" | "12-24" | "24+" | undefined;
-  }>({
-    genres: [],
-    episodes: undefined,
-  });
+	const [page, setPage] = useState(1);
+	const [filters, setFilters] = useState<{
+		genre: string | undefined;
+		episodes: "1-12" | "12-24" | "24+" | undefined;
+	}>({
+		genre: undefined,
+		episodes: undefined,
+	});
 
-  const router = useRouter();
-  const { loading, data, error } = useSectionAnimes(
-    typeof router.query.type === "string" ? router.query.type : "",
-    page
-  );
-  const filteredData = useMemo(() => {
-    let filteredAnimes = data?.data;
+	const router = useRouter();
+	const { loading, data, error } = useSectionAnimes(
+		typeof router.query.type === "string" ? router.query.type : "",
+		page
+	);
 
-    if (filters.episodes !== undefined) {
-      filteredAnimes = filteredAnimes?.filter((anime) => {
-        if (anime.episodes === null) {
-          return;
-        }
-        if (filters.episodes === "1-12") {
-          return anime.episodes >= 1 && anime.episodes <= 12;
-        } else if (filters.episodes === "12-24") {
-          return anime.episodes >= 12 && anime.episodes <= 24;
-        } else {
-          return anime.episodes >= 24;
-        }
-      });
-    }
+	const filteredData = useMemo(() => {
+		let filteredAnimes = data?.data;
 
-    if (filters.genres.length > 0) {
-      filteredAnimes = filteredAnimes?.filter((anime) => {
-        const currentAnimeGenres = getGenres(anime);
+		if (filters.episodes !== undefined) {
+			filteredAnimes = filteredAnimes?.filter((anime) => {
+				if (anime.episodes === null) {
+					return;
+				}
+				if (filters.episodes === "1-12") {
+					return anime.episodes >= 1 && anime.episodes <= 12;
+				} else if (filters.episodes === "12-24") {
+					return anime.episodes >= 12 && anime.episodes <= 24;
+				} else {
+					return anime.episodes >= 24;
+				}
+			});
+		}
 
-        for (const filterGenre of filters.genres) {
-          if (!currentAnimeGenres.includes(filterGenre)) {
-            return false;
-          }
-        }
+		if (filters.genre !== undefined) {
+			filteredAnimes = filteredAnimes?.filter((anime) => {
+				const currentAnimeGenres = getGenres(anime);
 
-        return true;
-      });
-    }
+				return currentAnimeGenres.includes(filters.genre!);
+			});
+		}
 
-    return filteredAnimes;
-  }, [data?.data, filters]);
+		return filteredAnimes;
+	}, [data?.data, filters]);
 
-  const onFilterChanged = (
-    filter:
-      | { type: "genres"; value: string | undefined }
-      | { type: "episodes"; value: typeof filters["episodes"] }
-  ) => {
-    if (filter.type === "episodes") {
-      setFilters((prev) => {
-        return { ...prev, episodes: filter.value };
-      });
-    } else {
-      setFilters((prev) => {
-        if (filter.value === undefined) {
-          return { ...prev, genres: [] };
-        }
+	const onFilterChanged = (
+		filter: { type: "genre"; value: string | undefined } | { type: "episodes"; value: typeof filters["episodes"] }
+	) => {
+		if (filter.type === "episodes") {
+			setFilters((prev) => {
+				return { ...prev, episodes: filter.value };
+			});
+		} else {
+			setFilters((prev) => {
+				return { ...prev, genre: filter.value };
+			});
+		}
+	};
 
-        return { ...prev, genres: [...prev.genres, filter.value] };
-      });
-    }
-  };
+	if (loading) {
+		return (
+			<FullScreen>
+				<Loader2 className="w-20 h-20 animate-spin text-aa-2 dark:text-aa-3" />
+			</FullScreen>
+		);
+	}
 
-  if (error !== "") {
-    return <p>{error}</p>;
-  }
+	if (error !== "") {
+		return (
+			<FullScreen>
+				<div>
+					<h1 className="text-3xl font-bold">{error}</h1>
+					<p>Try to refresh</p>
+				</div>
+			</FullScreen>
+		);
+	}
 
-  if (loading || data === undefined) {
-    return (
-      <div className="grid place-items-center h-full">
-        <Loader2 className="w-20 h-20 animate-spin" />
-      </div>
-    );
-  }
+	if (data === undefined) {
+		return (
+			<FullScreen>
+				<div>
+					<h1 className="text-3xl font-bold">Unable to fetch animes.</h1>
+					<p>Try to refresh</p>
+				</div>
+			</FullScreen>
+		);
+	}
 
-  return (
-    <div className="h-full">
-      {/* Filters */}
-      {/* By Type, Genre, Episode Group  */}
-      <div className="grid grid-cols-3 justify-center">
-        <Select
-          onValueChange={(value) => {
-            if (!isSection(value)) {
-              return;
-            }
+	return (
+		<div className="w-11/12 max-w-[1280px] md:w-2/3 m-auto">
+			<div className="grid grid-cols-3 justify-center gap-x-3 p-5">
+				<Select
+					value={typeof router.query.type === "string" ? router.query.type : ""}
+					onValueChange={(value) => {
+						if (!isSection(value)) {
+							return;
+						}
 
-            router.push(`/animes?type=${value}`);
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="airing">Airing</SelectItem>
-            <SelectItem value="popular">Popular</SelectItem>
-            <SelectItem value="upcoming">Upcoming</SelectItem>
-          </SelectContent>
-        </Select>
+						router.push(`/animes?type=${value}`);
+					}}
+				>
+					<SelectTrigger className="w-[100px] m-auto md:w-[150px]">
+						<SelectValue placeholder="Type" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value="airing">Airing</SelectItem>
+						<SelectItem value="popular">Popular</SelectItem>
+						<SelectItem value="upcoming">Upcoming</SelectItem>
+					</SelectContent>
+				</Select>
 
-        <Select
-          onValueChange={(value) => {
-            let genreValue: string | undefined = value;
+				<Select
+					value={filters.genre ?? "all"}
+					onValueChange={(value) => onFilterChanged({ type: "genre", value: value !== "all" ? value : undefined })}
+				>
+					<SelectTrigger className="w-[100px] m-auto md:w-[150px]">
+						<SelectValue placeholder="Genre" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value="all">All</SelectItem>
+						{getGenres(data.data).map((genre, idx) => {
+							return (
+								<SelectItem key={idx} value={genre}>
+									{genre}
+								</SelectItem>
+							);
+						})}
+					</SelectContent>
+				</Select>
 
-            if (value === "all") {
-              genreValue = undefined;
-            }
+				<Select
+					value={filters.episodes ?? "all"}
+					onValueChange={(value) => {
+						const isEpisodes = (value: string): value is "1-12" | "12-24" | "24+" => {
+							if (value === "1-12") {
+								return true;
+							}
+							if (value === "12-24") {
+								return true;
+							}
+							if (value === "24+") {
+								return true;
+							}
+							return false;
+						};
 
-            onFilterChanged({ type: "genres", value: genreValue });
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Genre" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="all">All</SelectItem>
-            {getGenres(data.data).map((genre, idx) => {
-              return (
-                <SelectItem key={idx} value={genre}>
-                  {genre}
-                </SelectItem>
-              );
-            })}
-          </SelectContent>
-        </Select>
+						onFilterChanged({
+							type: "episodes",
+							value: isEpisodes(value) ? value : undefined,
+						});
+					}}
+				>
+					<SelectTrigger className="w-[100px] m-auto md:w-[150px]">
+						<SelectValue placeholder="Episodes" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value="all">All</SelectItem>
+						<SelectItem value="1-12">1-12</SelectItem>
+						<SelectItem value="12-24">12-24</SelectItem>
+						<SelectItem value="24+">12+</SelectItem>
+					</SelectContent>
+				</Select>
+			</div>
 
-        <Select
-          onValueChange={(value) => {
-            console.log(value);
-            const isEpisodes = (
-              value: string
-            ): value is "1-12" | "12-24" | "24+" => {
-              if (value === "1-12") {
-                return true;
-              }
-              if (value === "12-24") {
-                return true;
-              }
-              if (value === "24+") {
-                return true;
-              }
-              return false;
-            };
-
-            onFilterChanged({
-              type: "episodes",
-              value: isEpisodes(value) ? value : undefined,
-            });
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Episodes" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="all">All</SelectItem>
-            <SelectItem value="1-12">1-12</SelectItem>
-            <SelectItem value="12-24">12-24</SelectItem>
-            <SelectItem value="24+">12+</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-
-      <AnimeResults data={filteredData ?? data.data} />
-    </div>
-  );
+			<AnimeResults data={filteredData ?? data.data} />
+		</div>
+	);
 }
