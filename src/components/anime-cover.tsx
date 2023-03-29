824c000fb6fc899e23465690c8e22c9fe90130bf
Giantte <gchambe2@emich.edu>
2023-03-28 20:49:08 -0400

    refactor: :recycle: refactor anime cover component for different fomats

--- src/components/anime-cover.tsx
+++ src/components/anime-cover.tsx
@@ -4,15 +4,28 @@ import { AspectRatio } from "./ui/aspect-ratio";
 type AnimeCoverProps = {
 	image: string;
 	name: string;
+	showFull?: true;
+	animeView?: true;
 };
 
-export default function AnimeCover({ image, name }: AnimeCoverProps) {
+export default function AnimeCover({ image, name, showFull, animeView }: AnimeCoverProps) {
 	return (
 		<div className="flex flex-col gap-y-1.5 justify-items-center text-center">
-			<AspectRatio ratio={2 / 3}>
-				<Image src={image} alt={`${name} image poster`} fill className="rounded-md object-cover" />
-			</AspectRatio>
-			<span>{name.length < 30 ? name : name.substring(0, 30)}</span>
+			{animeView ? (
+				<>
+					<h1 className="text-xl md:text-2xl">{name}</h1>
+					<AspectRatio ratio={2 / 3}>
+						<Image src={image} alt={`${name} image poster`} fill className="rounded-md object-cover" />
+					</AspectRatio>
+				</>
+			) : (
+				<>
+					<AspectRatio ratio={2 / 3}>
+						<Image src={image} alt={`${name} image poster`} fill className="rounded-md object-cover" />
+					</AspectRatio>
+					{showFull ? <span>{name}</span> : <span>{name.length < 30 ? name : name.substring(0, 30)}</span>}
+				</>
+			)}
 		</div>
 	);
 }
