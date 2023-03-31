2588a02ed7eeff1b1c9957aff5acfb63680abe78
gchamb <gianttechambers@gmail.com>
2023-03-30 22:26:42 -0400

    refactor: :recycle: checking for null values to display correct content

--- src/pages/animes/[id]/index.tsx
+++ src/pages/animes/[id]/index.tsx
@@ -1,4 +1,5 @@
 import AnimeCover from "@/components/anime-cover";
+import Chip from "@/components/ui/chip";
 import Ratings from "@/components/ui/ratings";
 import Jikan from "@/lib/jikan";
 
@@ -9,86 +10,106 @@ import { GetServerSideProps } from "next";
 const jikan = new Jikan();
 
 export default function Anime({ anime }: { anime: JikanAnime }) {
-	return (
-		<div className="w-11/12 h-full m-auto flex flex-col items-center pt-3 gap-5 lg:flex-row">
-			<div className="flex flex-col gap-y-4 items-center text-center lg:w-1/2 lg:m-auto">
-				<div className="w-[200px] xl:w-[250px]">
-					<AnimeCover image={anime.images.webp.image_url} name={anime.title} animeView />
-				</div>
+  return (
+    <div className="w-11/12 h-full m-auto flex flex-col items-center pt-3 gap-5 lg:flex-row">
+      <div className="flex flex-col gap-y-4 items-center text-center lg:w-1/2 lg:m-auto">
+        <div className="w-[200px] xl:w-[250px]">
+          <AnimeCover
+            image={anime.images.webp.image_url}
+            name={anime.title}
+            animeView
+          />
+        </div>
 
-				<div>
-					<h3>Community Rating</h3>
-                    {/* Check for null  */}
-					<Ratings value={Math.round(anime.score/ 2)} readOnly/>
-				</div>
-				<div>
-					<h1>List Button </h1>
-				</div>
-				<div className="grid grid-cols-3 justify-between">
-					<div>
-						<h4>Genres</h4>
-						<div className="flex gap-x-1 flex-wrap">
-							{getGenres(anime).map((genre, idx) => {
-								return (
-									<span className="text-sm" key={idx}>
-										{genre}
-									</span>
-								);
-							})}
-						</div>
-					</div>
-					<div>
-						<h4>Studios</h4>
-						<div>
-							{anime.studios.map((studio, idx) => {
-								return (
-									<span className="text-sm" key={idx}>
-										{studio.name}
-									</span>
-								);
-							})}
-						</div>
-					</div>
-					<div>
-						<h4>Aired In</h4>
-						<span className="text-sm">{anime.year ?? "N/A"}</span>
-					</div>
-				</div>
-			</div>
-			<div className="flex flex-col gap-y-2 text-center lg:w-1/2 lg:m-auto">
-				{/* Check if null */}
-				<iframe className="hidden aspect-video md:block" src={anime.trailer.embed_url} />
-				<div>
-					<h2 className="text-2xl underline underline-offset-4">Synopsis</h2>
-					{/* Check if null */}
-					<p>{anime.synopsis}</p>
-				</div>
-			</div>
-		</div>
-	);
+        {anime.score !== null && (
+          <div>
+            <h3>Community Rating</h3>
+            <Ratings value={Math.round(anime.score / 2)} readOnly />
+          </div>
+        )}
+
+        <div>
+          <h1>List Button </h1>
+        </div>
+        <div className="grid grid-flow-col auto-cols-fr justify-between gap-2">
+
+          {getGenres(anime).length > 0 && (
+            <div className="flex flex-col items-center">
+              <h4>Genres</h4>
+              <div className="flex gap-1 flex-wrap justify-center">
+                {getGenres(anime).map((genre, idx) => {
+                  return <Chip key={idx} text={genre} />;
+                })}
+              </div>
+            </div>
+          )}
+
+          {anime.studios.length > 0 && (
+            <div>
+              <h4>Studios</h4>
+              <div className="flex gap-1 flex-wrap justify-center">
+                {anime.studios.map((studio, idx) => {
+                  return <Chip key={idx} text={studio.name} />;
+                })}
+              </div>
+            </div>
+          )}
+
+          <div>
+            <h4>Aired In</h4>
+            <div className="flex gap-1 flex-wrap justify-center">
+              <Chip text={anime.year?.toString() ?? "N/A"} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`flex flex-col gap-y-2 text-center lg:w-1/2 lg:m-auto ${
+          anime.trailer.embed_url === null && anime.synopsis === null
+            ? "hidden"
+            : ""
+        }`}
+      >
+        {anime.trailer.embed_url !== null && (
+          <iframe
+            className="hidden aspect-video md:block"
+            src={anime.trailer.embed_url}
+          />
+        )}
+
+        {anime.synopsis !== null && (
+          <div>
+            <h2 className="text-2xl underline underline-offset-4">Synopsis</h2>
+            <p>{anime.synopsis}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-	if (params === undefined) {
-		return {
-			notFound: true,
-		};
-	}
+  if (params === undefined) {
+    return {
+      notFound: true,
+    };
+  }
 
-	const malId = Number(params.id);
-	if (isNaN(malId) || (!isNaN(malId) && malId < 1)) {
-		return {
-			notFound: true,
-		};
-	}
+  const malId = Number(params.id);
+  if (isNaN(malId) || (!isNaN(malId) && malId < 1)) {
+    return {
+      notFound: true,
+    };
+  }
 
-	// make sure to cache this page for a day
+  // make sure to cache this page for a day
 
-	const { data: anime } = await jikan.getAnime(malId);
+  const { data: anime } = await jikan.getAnime(malId);
 
-	return {
-		props: {
-			anime,
-		},
-	};
+  return {
+    props: {
+      anime,
+    },
+  };
 };
