35142e81da4db1cff12a64572bbc59bf3d22dc84
Giantte <gchambe2@emich.edu>
2023-03-31 20:46:02 -0400

    feat: :sparkles: add navigation

---
+++ src/components/ui/navigation.tsx
@@ -0,0 +1,45 @@
+import Account from "../Account";
+import Link from "next/link";
+
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/router";
+
+function BackButton() {
+	const router = useRouter();
+
+	return (
+		<div className="flex flex-col">
+			<div className="flex items-center gap-2 cursor-pointer hover:text-aa-4 dark:hover:text-aa-3">
+				<ArrowLeft onClick={() => router.back()} />
+				<span className="text-sm">Back</span>
+			</div>
+			<Link href="/" className="self-end text-xs hover:text-aa-4 hover:underline hover:underline-offset-2 dark:hover:text-aa-3">
+				or back to home
+			</Link>
+		</div>
+	);
+}
+
+export default function Navigation() {
+	const router = useRouter();
+
+	return (
+		<nav className="flex w-11/12 m-auto items-center p-2">
+			<div>
+				{router.pathname !== "/" && (
+					// <ArrowLeft className="cursor-pointer hover:text-aa-4" onClick={() => router.back()} />
+					<BackButton />
+				)}
+			</div>
+			<div className="ml-auto flex items-center gap-x-8">
+				{router.pathname !== "/browse" && (
+					<Link href="/browse" className="font-semibold cursor-pointer hover:text-aa-4 dark:hover:text-aa-3">
+						Browse
+					</Link>
+				)}
+
+				<Account />
+			</div>
+		</nav>
+	);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Navigation from "@/components/ui/navigation";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
@@ -7,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.className} h-screen`}>
+      <Navigation />
       <Component {...pageProps} />
     </main>
   );
