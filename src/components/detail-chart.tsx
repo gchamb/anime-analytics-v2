af28adf29b61811ed7624592c33f2e4e035b0841
Giantte <gchambe2@emich.edu>
2023-06-12 15:39:11 -0400

    feat: :iphone: make detail view responsive

--- src/components/detail-chart.tsx
+++ src/components/detail-chart.tsx
@@ -10,7 +10,7 @@ export default function DetailChart({ title, data }: DetailChartProps) {
 	return (
 		<>
 			{data.length > 0 && (
-				<div className="w-1/5 mx-auto min-h-[250px] p-2 rounded border bg-aa-1 border-aa-dark-1 dark:border-aa-2 dark:bg-aa-dark-1">
+				<div className="w-full mx-auto min-h-[250px] p-2 rounded border bg-aa-1 border-aa-dark-1 dark:border-aa-2 dark:bg-aa-dark-1 md:w-1/3 lg:w-1/5">
 					<h1 className="text-2xl text-center mb-4">{title}</h1>
 					<div className="grid grid-cols-4 gap-1.5">
 						{data.map((anime) => {
