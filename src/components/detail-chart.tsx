6e7392d7a320825ff868a6d2bc483cab4f4faebc
Giantte <gchambe2@emich.edu>
2023-07-02 14:56:57 -0500

    fix height on detail chart

--- src/components/detail-chart.tsx
+++ src/components/detail-chart.tsx
@@ -9,7 +9,7 @@ export default function DetailChart({ title, data }: DetailChartProps) {
 	return (
 		<>
 			{data.length > 0 && (
-				<div className="w-full mx-auto min-h-[250px] p-2 rounded border bg-aa-1 border-aa-dark-1 dark:border-aa-2 dark:bg-aa-dark-1 md:w-1/3 lg:w-1/5">
+				<div className="w-full mx-auto min-h-[250px] max-h-[500px] p-2 rounded border bg-aa-1 border-aa-dark-1 dark:border-aa-2 dark:bg-aa-dark-1 md:w-1/3 lg:w-1/5">
 					<h1 className="text-2xl text-center mb-4">{title}</h1>
 					<div className="grid grid-cols-4 gap-1.5">
 						{data.map((anime) => {
